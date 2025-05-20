997f5bb01249d059f1ddcd5ca47ec5aa30fe7e59
ElectricWhelk <leahfrancesca64@gmail.com>
2025-05-20 19:22:54 +0100

    fucked around with python

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -1,9 +1,55 @@
-import random
+def decorator(func):
+    def wrapper(self):
+        top = "wrapping...\n"
+        mid = func(self)
+        bottom = "\nwrapped!"
+        return top + mid[0] + bottom
+    return wrapper
 
-def generate(x):
-    diceRoll = random.randrange(0, x)
-    strMax = str(x)
-    strDice = str(diceRoll)
-    print("Alright, here's a number between 0 and " + strMax + ":\n" + strDice)
+class Musician:
+    def __init__(self, name , genre, magnumOpus):
+        self._name = name
+        self._genre = genre
+        self._magnumOpus = magnumOpus
 
-generate(10)
+    def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
+        print("getting name...")
+        return self._name
+
+    def getMembers(self):
+        return self.name
+
+class Band(Musician):
+    def __init__(self, name, genre, magnumOpus, members):
+        super().__init__(name, genre, magnumOpus)
+        self.members = members
+
+
+    def __iter__(self):
+        self.pos = 0
+        return self
+
+    def __next__(self):
+        length = len(self.members)
+        try:
+            output = self.members[self.pos]
+            self.pos += 1
+            return output
+        except:
+            raise StopIteration
+
+    @decorator
+    def getMembers(self):
+        return self.members
+
+
+playlist = [
+    Musician("Joanna Newsom", "Indie Folk", "Divers"),
+    Band("Fugazi", "Post-Hardcore", "Red Medicine", ["Ian MacKaye", "Guy Picciotto", "Joe Lally", "Brendan Canty"])
+       ]
+
+print(playlist[0].name)
